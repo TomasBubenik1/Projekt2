5082397f8b1b24bed88dc5b59299a7f9ad95c1a3
bubeniktomi@gmail.com <bubeniktomi@gmail.com>
2023-04-19 20:48:11 +0200

    Funkcny login a udaje o uzivatelovy

--- src/app/hlavnastranka/hlavnastranka.component.ts
+++ src/app/hlavnastranka/hlavnastranka.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { IUser } from '../services/user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-hlavnastranka',
   templateUrl: './hlavnastranka.component.html',
@@ -11,11 +12,12 @@ export class HlavnastrankaComponent implements OnInit {
   loggedInUser = this.userservice.loggedInUser;
   loginTime ! : string;
   clickCounter = 0;
+  
   incrementCounter() {
     this.clickCounter++;
   }
   
-  constructor(private userservice: UserService) {}
+  constructor(private userservice: UserService, private router:Router) {}
 
   ngOnInit(): void {
 
@@ -23,5 +25,10 @@ export class HlavnastrankaComponent implements OnInit {
     document.addEventListener('click', this.incrementCounter.bind(this));
   }
   
+  logOut(){this.router.navigate(['/login']);
+  const loginTime = new Date();
+  localStorage.removeItem('loginTime');
+}
+
 }
 const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser') || '{}') as IUser;
